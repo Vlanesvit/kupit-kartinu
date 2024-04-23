dadffd011ff2a1970c3e1777a00480c18e92b875
Vlad Nesterov <53149984+Vlanesvit@users.noreply.github.com>
2024-04-23 13:34:47 +0800

    load 2 page

--- js/timer.js
+++ js/timer.js
@@ -30,15 +30,29 @@ document.addEventListener('DOMContentLoaded', function () {
 			const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
 			const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
 			const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
-			const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
 			$days.textContent = days;
 			$hours.textContent = hours;
 			$minutes.textContent = minutes;
-			$days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
-			$hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
-			$minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
-		}
 
+			// проверяем на наличие атриюута, если его нет - выводим склоняемые надписи
+			if (!$days.hasAttribute('data-title')) {
+				$days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
+			}
+			if (!$hours.hasAttribute('data-title')) {
+				$hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+			}
+			if (!$minutes.hasAttribute('data-title')) {
+				$minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+			}
+
+			// если осталось 0 дней, то блок скрывается
+			if ($days.textContent === '0') {
+				$days.style.display = 'none'
+			}
+			if ($hours.textContent === '0') {
+				$hours.style.display = 'none'
+			}
+		}
 		// получаем элементы, содержащие компоненты даты
 		const $days = timer.querySelector('.timer__days');
 		const $hours = timer.querySelector('.timer__hours');
